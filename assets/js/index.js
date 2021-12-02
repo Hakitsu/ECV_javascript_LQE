98b185993e3bc124f0b61dcd796156c9a8b2759c
Quentin Moras <quentin.moras@gmail.com>
2021-12-02 17:55:03 +0100

    Merge 17H54

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+newsletter_check = false;
+$(function(){
+    $("body").on("mouseleave",function(){
+        if(newsletter_check == false){
+            exitPage();
+            newsletter_check = true;
+        }
+    })
+})
+
+function exitPage(){
+    Swal.fire({
+        title : 'Newsletter',
+        text : 'Abonnez-vous pour ne pas louper nos news',
+        input : 'email',
+        showCancelButton: true,
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'NON !',
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: "S'ABONNER !",
+        reverseButtons: true
+    }).then((result) => {
+    if (result.isConfirmed) {
+        Swal.fire(
+        'Merci!',
+        'Vous allez recevoir un mail qui confirme votre abonnement.',
+        'success'
+        )
+    }
+    });
+}
