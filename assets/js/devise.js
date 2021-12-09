c6dd42e5737706a1ceeb336a5d15c28f7029757d
Eloicau <eloi.caudrillier@gmail.com>
2021-12-09 18:04:50 +0100

    change fonctionnel sur Boutique et Produit

--- assets/js/devise.js
+++ assets/js/devise.js
@@ -1,7 +1,13 @@
-$(".imgDevise").click(function(){
+$(".bouton_change").click(function(){
 
     if($(this).attr("devise") == "euro"){
         $('.price').each(function(i){
+            var prix = ($(this).data("value") * 0.89).toFixed(0); 
+            $(this).text(prix + " €");
+           
+        });
+
+        $('.prix_produit').each(function(i){
             var prix = ($(this).data("value") * 0.89).toFixed(0); 
             $(this).text(prix + " €");
            
@@ -13,9 +19,21 @@ $(".imgDevise").click(function(){
             $(this).text(prix + " $");
            
         });
+
+        $('.prix_produit').each(function(i){
+            var prix = $(this).data("value"); 
+            $(this).text(prix + " $");
+           
+        });
     }
     if($(this).attr("devise") == "yen"){
         $('.price').each(function(i){
+            var prix = ($(this).data("value") * 113.5).toFixed(0); 
+            $(this).text(prix + " ¥");
+           
+        });
+
+        $('.prix_produit').each(function(i){
             var prix = ($(this).data("value") * 113.5).toFixed(0); 
             $(this).text(prix + " ¥");
            
