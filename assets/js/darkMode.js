3e11d06b041201c8c274fecf375a462db613b323
Lea Renaux <lea.renaux@mail-ecv.fr>
2021-12-09 17:12:59 +0100

    add css darkmode boutique

--- assets/js/darkMode.js
+++ assets/js/darkMode.js
@@ -1,27 +1,47 @@
 let buttonDarkMode = $('.buttonDarkMode');
 let SectionàPropos = $('#propos');
-let SectionàProposH1 = $('#propos h1');
+let SectionàProposH1 = $('#propos h1 ');
+let SectionBoutiqueH1 = $('#Boutique h1');
 let feedbackInput = $('.feedback-input');
+let boutique = $('#Boutique');
+let card = $('.card button')
 
 buttonDarkMode.on('click', function() {
     if(buttonDarkMode.hasClass('darkMode')){
     buttonDarkMode.text("Activer le dark mode ")
-    
+
     buttonDarkMode.removeClass('darkMode')
     SectionàPropos.removeClass('darkMode')
     SectionàProposH1.removeClass('darkMode')
-    $('body').removeClass('darkMode')
-    feedbackInput.removeClass('darkMode')
     SectionàProposH1.addClass('divider gradient')
+    card.addClass('darkMode')
+    $('body').removeClass('darkMode')
+    $('html').removeClass('darkMode')
+
+    feedbackInput.removeClass('darkMode')
+
+    boutique.removeClass('darkMode')
+    SectionBoutiqueH1.removeClass('darkMode')
+    SectionBoutiqueH1.addClass('divider gradient')
     }
     else{
         buttonDarkMode.text("Désactiver le dark mode ")
         
+        $('body').addClass('darkMode')
+        $('html').addClass('darkMode')
+        
         buttonDarkMode.addClass('darkMode')
+
         SectionàPropos.addClass('darkMode')
         SectionàProposH1.addClass('darkMode')
-        feedbackInput.addClass('darkMode')
         SectionàProposH1.removeClass('divider gradient')
+        card.removeClass('darkMode')
+        feedbackInput.addClass('darkMode')
+
+        boutique.addClass('darkMode')
+        SectionBoutiqueH1.removeClass('divider gradient')
+        SectionBoutiqueH1.addClass('darkMode')
+       
         $('body').addClass('darkMode')
         
 
